644e3fff02f84ee6e44dc9650c48392b27a037e8
KnefiCZ <matej.kneifl@seznam.cz>
2020-02-04 15:45:05 +0100

    Doba vec maturitaaaaaaaaaaa
    wooooooooooooo maturitaaaaaaXD

---
+++ add_something.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add_something</title>
+</head>
+<body>
+    <form action="submit.php" method="post">
+    <label for="email">Email:</label>
+    <input type="email" name="email" placeholder="priklad@abc.cz"> <br>
+    <label for="password">Heslo:</label>
+    <input type="password" name="password" placeholder="******"> <br>
+    <label for="firstname">Jméno:</label>
+    <input type="text" name="firstname" placeholder="Honza"> <br>
+    <label for="lastname">Příjmení:</label>
+    <input type="text" name="lastname" placeholder="Nový"> <br>
+    <label for="address">Adresa:</label>
+    <input type="text" name="address" placeholder="Praha 10, 29004/byt 56"> <br>
+    <label for="city">Město:</label>
+    <input type="text" name="city" placeholder="Praha"> <br>
+    <input type="submit" name="sub" > 
+    </form>
+    
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,8 +1,39 @@
+<?php //Načtení všech knihoven z adresáře VENDOR
+    require_once "vendor/autoload.php";
 
-<?php require_once "header.php";?>
+    use Illuminate\Database\Capsule\Manager as DB;
+
+    $db = new DB;
+    $db->addConnection(
+        [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'beast_restaurant',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci'
+        ]
+        );
+
+    $db->setAsGlobal();
+    $db->bootEloquent();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Select vseho</title>
+</head>
+<body>
+<!-- Pokus o jakoze incert :DDDDDDDDD --> 
+<?//php require_once "header.php";?>
 
 
     <?php
+    
     try {
         $products = DB::select("SELECT * FROM products;");
     } catch (\Throwable $th) {
@@ -18,8 +49,32 @@
             echo $product->id_type . "<br>";
             echo $product->weight . "<br>";
         }
+    ?>
+    <form action="#">
+        <input type="submit" value="Přidat další do PRODUCTS.">
+    </form>
+    <hr>
+    <?php
+    try {
+        $users = DB::select("SELECT * FROM users;");
+    } catch (\Throwable $th) {
+        echo "Nepovedl se SELECT z users!" . "<br>";
+        $users = array();
+        var_dump($th);
+    }
+        foreach ($users as $user) {
+            echo $user->id_user . "<br>";
+            echo $user->email . "<br>";
+            echo $user->password . "<br>";
+            echo $user->firstname . "<br>";
+            echo $user->lastname . "<br>";
+            echo $user->address . "<br>";
+            echo $user->city . "<br>";
+        }
 
     ?>
-    
+    <form action="add_something.php">
+        <input type="submit" value="Přidat další do USERS.">
+    </form>
 </body>
 </html>
